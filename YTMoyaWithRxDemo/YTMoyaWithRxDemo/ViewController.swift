3da9decd5367b27cfaf69a34b310c0ad9cb8cd2a
Ahmed Ashraf Fayeq <ahmedashraffayeq@gmail.com>
2022-11-11 17:30:54 +0300

    -create the project -add MoyaRxSwift pod

---
+++ YTMoyaWithRxDemo/YTMoyaWithRxDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YTMoyaWithRxDemo
+//
+//  Created by Ahmed Fayek on 17/04/1444 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
