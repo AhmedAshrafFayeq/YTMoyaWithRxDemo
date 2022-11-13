d77081490d4e3856e1c92b6985494903b18e8f77
Ahmed Ashraf Fayeq <ahmedashraffayeq@gmail.com>
2022-11-13 09:28:59 +0300

    calling getAllUsers API

--- YTMoyaWithRxDemo/YTMoyaWithRxDemo/ViewController.swift
+++ YTMoyaWithRxDemo/YTMoyaWithRxDemo/ViewController.swift
@@ -6,17 +6,21 @@
 //
 
 import UIKit
+import RxSwift
 
 class ViewController: UIViewController {
     
     private var viewModel = ViewModel()
+    var bag = DisposeBag()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         viewModel.fetchUser(id: "2")
-        // Do any additional setup after loading the view.
+        
+        viewModel.userResponse.subscribe { userResponse in
+            print(userResponse.element)
+        }.disposed(by: bag)
+        
+        viewModel.fetchAllUsers()
     }
-
-
 }
-
